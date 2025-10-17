d4f3817a47839d906b84168cd3d8e91f4cca5df4
mirkobechini <mirkobechini@gmail.com>
2025-10-17 14:19:14 +0200

    Add AboutPage, CharactersPage, and ContactsPage components

---
+++ src/pages/CharactersPage.jsx
@@ -0,0 +1,101 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
+export default function CharactersPage( ) {
+    
+    const [characters, setCharacters] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+
+    function fetchData(page) {
+        console.log('fetch data');
+        //fetch data with axios
+        axios.get(`https://rickandmortyapi.com/api/character?page=${page}`)
+            .then(res => {
+                const { info, results } = res.data
+
+                setCharacters(results)
+
+            })
+            .catch(err => {
+                console.error(err.message);
+            }
+            )
+        //log fetched data
+    }
+
+    useEffect(() => {
+        fetchData(currentPage)
+    }, [])
+
+
+
+    return (
+        <>
+            <main>
+                <div className="p-5 mb-4 bg-light rounded-3">
+                    <div className="p-5 mb-4 bg-light rounded-3">
+                        <div className="container-fluid py-5">
+                            <h1 className="display-5 fw-bold">Rick and Morty characters</h1>
+                            <p className="col-md-8 fs-4">
+                                Here is your characters list
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <section id="characters">
+                    <div className="container">
+                        <div className="row">
+                            <nav aria-label="Page navigation">
+                                <ul className="pagination">
+                                    <li className="page-item disabled">
+                                        <button className="page-link" onClick={fetchData} aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                        </button>
+                                    </li>
+                                    <li className={`page-item`} aria-current="page">
+                                        <button className="page-link" value="1" onClick={fetchData} aria-label="1">
+                                            <span aria-hidden="true">1</span>
+                                        </button>
+                                    </li>
+                                    <li className="page-item">
+                                        <button className="page-link" value="2" onClick={fetchData} aria-label="2">
+                                            <span aria-hidden="true">2</span>
+                                        </button>
+                                    </li>
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={fetchData} value="next" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="row">
+                            {characters.map(character =>
+
+                                <div className="col-12 col-sm-6 col-md-4 mb-4" key={character.id}>
+                                    <div className={`card h-100  position-relative ${character.status === "Alive" ? "border-success bg-success-subtle" : "border-danger bg-danger-subtle"}`}>
+                                        <img className='img-fluid' src={character.image} alt="" />
+                                        <div className="card-body">
+                                            <h3>{character.name}</h3>
+                                            <span>Origin: {character.origin.name} </span>
+                                            <div className="badge bg-primary position-absolute top-0 start-0">
+                                                {character.species}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+
+                    </div>
+                </section>
+            </main >
+
+
+        </>
+    )
+}
---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,7 @@
+export default function ContactsPage(){
+    return(
+        <main>
+            <h1>Contacts us</h1>
+        </main>
+    )
+}
