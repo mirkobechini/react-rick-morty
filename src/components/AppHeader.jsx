8f80a1116c899628211e1fb25b16178751979a00
mirkobechini <mirkobechini@gmail.com>
2025-10-17 13:41:10 +0200

    Refactor AppHeader component structure for improved layout and readability

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,14 +1,25 @@
-export default function AppHeader({fetchData}){
-    return(
+export default function AppHeader({ fetchData }) {
+    return (
         <header>
-             <div className="container d-flex justify-content-between align-items-center">
-          <div className="logo">
-            <strong>Rick e Morty</strong>
-          </div>
-          <button className='btn btn-dark' value="1" onClick={fetchData}>
-            <i className="bi bi-download"></i> Fetch data
-          </button>
-        </div>
+            <nav className="container d-flex justify-content-between align-items-center">
+                <div className="logo">
+                    <strong>Rick e Morty</strong>
+                </div>
+                <ul className="list-unstyled d-flex gap-2 mt-2">
+                    <li>
+                        <a className="nav-link" href="">Characters</a>
+                    </li>
+                    <li>
+                        <a className="nav-link" href="">About</a>
+                    </li>
+                    <li>
+                        <a className="nav-link" href="">Contacts</a>
+                    </li>
+                </ul>
+                <button className='btn btn-dark' value="1" onClick={fetchData}>
+                    <i className="bi bi-download"></i> Fetch data
+                </button>
+            </nav>
         </header>
     )
 }
