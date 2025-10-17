ea3ee98241f8ee6f642612eeb704b655f837e7ce
mirkobechini <mirkobechini@gmail.com>
2025-10-17 14:56:56 +0200

    Refactor CharacterCard and CharacterPage components for improved layout and styling

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,6 +1,6 @@
 export default function CharacterCard({character}){
     return(
-        <div className="card col-4 mx-auto p-3 text-center justify-content-center align-items-center">
+        <div className=" characterCard card col-12 col-sm-5 col-md-3 mx-auto p-3 text-center justify-content-center align-items-center " data-bs-theme='dark'>
             <img className="img-fluid rounded-circle" width={200} src={character.image} alt= {character.name} />
             <h3> {character.name} </h3> 
             <div> Specie: {character.species} </div>
--- src/pages/CharacterPage.jsx
+++ src/pages/CharacterPage.jsx
@@ -3,32 +3,44 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import CharacterCard from "../components/CharacterCard"
 
-export default function CharacterPage(){
-    const {id} = useParams()
+export default function CharacterPage() {
+    const { id } = useParams()
     const [character, setCharacter] = useState(null)
 
     const endpoint = `https://rickandmortyapi.com/api/character/${id}`
-    
 
-    useEffect(() =>{
+
+    useEffect(() => {
         fetchData(endpoint)
     }, [])
 
 
-    function fetchData(endpoint){
+    function fetchData(endpoint) {
         axios.get(endpoint)
-        .then(res=> {       
-            console.log(res.data);
-            setCharacter(res.data)
-        })
-        .catch(err =>{
-            console.error(err);
-        })
+            .then(res => {
+                console.log(res.data);
+                setCharacter(res.data)
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
 
-    return(
+    return (
         <div>
-            {character != null ? <CharacterCard character = {character} /> : <div> loading...</div>}
+            <div className="p-5 bg-light">
+                <div className="p-5 mb-4 bg-light rounded-3">
+                    <div className="container-fluid py-5">
+                        <h1 className="display-5 fw-bold">Discover The character</h1>
+                        <p className="col-md-8 fs-4">
+                            Here is your characters list
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            {character != null ? <CharacterCard character={character} /> : <div> loading...</div>}
         </div>
     )
 }
