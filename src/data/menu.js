0a2906e01756513e262b8188c9e66a7056718e4b
mirkobechini <mirkobechini@gmail.com>
2025-10-17 14:26:26 +0200

    Add menu data structure for navigation links

---
+++ src/data/menu.js
@@ -0,0 +1,23 @@
+const menu = [
+    {
+        id: 1,
+        text: 'Home',
+        link: '/'
+    },
+    {
+        id: 2,
+        text: 'Characters',
+        link: '/characters'
+    },
+    {
+        id: 3,
+        text: 'About',
+        link: '/about'
+    },
+    {
+        id: 4,
+        text: 'Contacts',
+        link: '/contacts'
+    }
+]
+export default menu
