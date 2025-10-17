38fe90246ed0ef28c2eb643fe27cfb4277c8785d
mirkobechini <mirkobechini@gmail.com>
2025-10-17 14:19:09 +0200

    Refactor AppHeader component to use NavLink for navigation and streamline menu structure

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,30 @@
-export default function AppHeader({ fetchData }) {
+import {NavLink} from "react-router-dom"
+
+export default function AppHeader() {
+    
+    const menu = [
+        {
+            id:1,
+            text:'Home',
+            link:'/'
+        },
+        {
+            id:2,
+            text:'Characters',
+            link:'/characters'
+        },
+        {
+            id:3,
+            text:'About',
+            link:'/about'
+        },
+        {
+            id:4,
+            text:'Contacts',
+            link:'/contacts'
+        }
+    ]
+
     return (
         <header>
             <nav className="container d-flex justify-content-between align-items-center">
@@ -6,19 +32,15 @@ export default function AppHeader({ fetchData }) {
                     <strong>Rick e Morty</strong>
                 </div>
                 <ul className="list-unstyled d-flex gap-2 mt-2">
-                    <li>
-                        <a className="nav-link" href="">Characters</a>
+                   
+                   {menu.map(item =>
+
+                    <li key={item.id}>
+                        <NavLink className="nav-link" to={item.link}>{item.text}</NavLink>
                     </li>
-                    <li>
-                        <a className="nav-link" href="">About</a>
-                    </li>
-                    <li>
-                        <a className="nav-link" href="">Contacts</a>
-                    </li>
+                   )}
+
                 </ul>
-                <button className='btn btn-dark' value="1" onClick={fetchData}>
-                    <i className="bi bi-download"></i> Fetch data
-                </button>
             </nav>
         </header>
     )
